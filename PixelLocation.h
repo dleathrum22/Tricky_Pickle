dd5583bb78c0c2d6bf8a378a8ceef543e7d12cb9
dleathrum22 <leathd@rpi.edu>
2016-11-04 14:47:08 -0400

    Update and rename PixelLocation.ino to PixelLocation.h

---
+++ PixelLocation.h
@@ -0,0 +1,10 @@
+#include Adafruit_Neopixel.h
+
+class PixelLocation {
+  public:
+    PixelLocation(double Angle1, double Angle2, uint8_t r, uint8_t g, uint8_t b);
+    PixelLocation(double Angle, uint8_t r, uint8_t g, uint8_t b);
+  private:
+    int numPixels;
+    int curve;
+}
--- PixelLocation.ino
+++
@@ -1,5 +0,0 @@
-#include Neopixel
-
-class PixelLocation {
-
-}
