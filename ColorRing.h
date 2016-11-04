71a4605a0e74616466f2a3c420bf499c310f071a
dleathrum22 <leathd@rpi.edu>
2016-11-04 15:01:19 -0400

    Update ColorRing.h

--- ColorRing.h
+++ ColorRing.h
@@ -2,7 +2,12 @@
 
 class ColorRing {
   public:
-  
+    ColorRing(int lightInitial, int lineLength, bool overwriteLap, bool rotation, int flashSpeed);
+    
   private:
-  
+    int angle1;
+    int angle2;
+    uint8_t r;
+    uint8_t g; 
+    uint8_t b;
 }
