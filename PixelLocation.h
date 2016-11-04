0dd32b148e519113bfa3fa4a6e2a47323c801345
dleathrum22 <leathd@rpi.edu>
2016-11-04 15:03:27 -0400

    Update PixelLocation.h

--- PixelLocation.h
+++ PixelLocation.h
@@ -1,12 +1,14 @@
-#include Adafruit_Neopixel.h
+#include "Adafruit_Neopixel.h"
 
 class PixelLocation {
   public:
     PixelLocation(double Angle1, double Angle2, uint8_t r, uint8_t g, uint8_t b);
     PixelLocation(double Angle, uint8_t r, uint8_t g, uint8_t b);
+  
   private:
     int numPixels;
     int curve;
     int location;
     int angle;
+  
 }
