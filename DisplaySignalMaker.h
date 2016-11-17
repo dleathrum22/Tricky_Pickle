c0e0447dfa1168fc44345d20aa048d3bba9e1492
dleathrum22 <leathd@rpi.edu>
2016-11-17 18:36:57 -0500

    Update DisplaySignalMaker.h

--- DisplaySignalMaker.h
+++ DisplaySignalMaker.h
@@ -3,8 +3,11 @@
 class DisplaySignalMaker {
   public:
     DisplaySignalMaker(); //When the input data is determined as to what is recieved via bluethooth, put them as a parameter here
-    // This class is going to be a bitch
+    Heading(int heading);
+    Accelerameter(int x1, int y1, int z1);
   private:
     //display signals should be stored here
     bool trigger;
+    int x, y, z;
+    int heading;
 }
