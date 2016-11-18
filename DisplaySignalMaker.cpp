a235d15e0670db2b14086495c6df1c4e3f30378d
dleathrum22 <leathd@rpi.edu>
2016-11-17 23:23:55 -0500

    Update DisplaySignalMaker.cpp

--- DisplaySignalMaker.cpp
+++ DisplaySignalMaker.cpp
@@ -1,1 +1,34 @@
-That thing
+#include "DisplaySignalMaker.h"
+#include "PixelLocation.h"
+
+DisplaySignalMaker::DisplaySignalMaker() {
+  pixie = new PixelLocation();
+  pixie.clear();
+  pixie.show();
+}
+
+void DisplaySignalMaker::Heading( int heading1 ) {
+  if (heading1 == heading) {
+    pixie.show();
+    return;
+  }
+  pixie.clear();
+  heading = heading1;
+  
+  int angle = heading + 270;
+  while (angle > 360) { angle -= 360; }
+  pixie.dot(angle, unit32_t 0/* make this red */);
+  return;
+}
+
+void DisplaySignalMaker::Accelerameter( int x1, int y1 ) {
+  if ( (x1 == x) && (y1 == y) ) {
+    pixie.show();
+    return;
+  }
+  x = x1;
+  y = y1;
+  pixie.clear();
+  
+  
+}
