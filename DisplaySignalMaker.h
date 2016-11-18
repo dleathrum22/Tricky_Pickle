e24bde59d52c05a64fa6129fc0f24c95b9dc914a
dleathrum22 <leathd@rpi.edu>
2016-11-17 23:24:07 -0500

    Update DisplaySignalMaker.h

--- DisplaySignalMaker.h
+++ DisplaySignalMaker.h
@@ -1,13 +1,15 @@
-
+#include "PixelLocation.h"
 
 class DisplaySignalMaker {
   public:
     DisplaySignalMaker(); //When the input data is determined as to what is recieved via bluethooth, put them as a parameter here
-    Heading(int heading);
-    Accelerameter(int x1, int y1, int z1);
+    void Heading(int heading1); //Sets a red light to always point to the magnetic North Pole
+    void Accelerameter(int x1, int y1);
   private:
     //display signals should be stored here
     bool trigger;
-    int x, y, z;
-    int heading;
+    PixelLocation pixie;
+    int x, y;
+    int heading, angle;
+    int angle1, angle2;
 }
