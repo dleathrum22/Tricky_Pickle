cc874124feea009c93f50bdaf2dab9a49130d932
dleathrum22 <leathd@rpi.edu>
2016-11-20 22:43:32 -0500

    Update DisplaySignalMaker.h

--- DisplaySignalMaker.h
+++ DisplaySignalMaker.h
@@ -5,11 +5,12 @@ class DisplaySignalMaker {
     DisplaySignalMaker(); //Sets up the PixelLocation class to be ready to alter the lights
     void Heading(int heading1); //Sets a red light to always point to the magnetic North Pole
     void Accelerameter(int x1, int y1);
+    void Turn(bool isLeft, bool isRight, bool isFast);
   private:
     //display signals should be stored here
     bool trigger;
     PixelLocation pixie;
     int x, y;
     int heading, angle;
-    int angle1, angle2;
+    int angle, angle1, angle2;
 }
