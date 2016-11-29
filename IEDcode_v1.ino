1987d2b969cd3733834c1f8b8cf8404de7800fdd
dleathrum22 <leathd@rpi.edu>
2016-11-29 14:44:12 -0500

    Update IEDcode_v1.ino

--- IEDcode_v1.ino
+++ IEDcode_v1.ino
@@ -8,47 +8,47 @@ void setup() {
   
   //constant variables
   DisplaySignalMaker signal = new DisplaySignalMaker();
-  int interrupt;
+  int timer = timer(0);
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  while (interrupt < 10);
-  while ( (interrupt >= 10) && (interrupt < 21)) {
+  while (timer < 10);
+  while ( (timer >= 10) && (timer < 21)) {
     signal.Turn(false, true, true, unit8_t 00000000, unit8_t 11111111, unit8_t 00000000);
   }
-  while ( (interrupt >= 22) && (interrupt < 28) ) {
+  while ( (timer >= 22) && (timer < 28) ) {
     signal.Turn(true, false, false, unit8_t 00000000, unit8_t 11111111, unit8_t 00000000);
   }
-  while ( (interrupt >= 28) && (interrupt < 36) ) {
+  while ( (timer >= 28) && (timer < 36) ) {
     signal.Turn(true, false, true, unit8_t 00000000, unit8_t 11111111, unit8_t 00000000);
   }
-  while (interrupt < 37);
-  while ( (interrupt >= 37) && (interrupt < 40) ) {
+  while (timer < 37);
+  while ( (timer >= 37) && (timer < 40) ) {
     signal.Turn(true, false, true, unit8_t 00000000, unit8_t 11111111, unit8_t 00000000);
   }
-  while ( (interrupt >= 40) && (interrupt < 52) ) {
+  while ( (timer >= 40) && (timer < 52) ) {
     signal.Turn(false, true, true, unit8_t 00000000, unit8_t 11111111, unit8_t 00000000);
   }
-  while (interrupt < 54);
-  while ( (interrupt >= 54) && (interrupt < 61) ) {
+  while (timer < 54);
+  while ( (timer >= 54) && (timer < 61) ) {
     signal.Turn(false, true, false, unit8_t 11111111, unit8_t 11111111, unit8_t 00000000);
   }
-  while ( (interrupt >= 61) && (interrupt < 69) ) {
+  while ( (timer >= 61) && (timer < 69) ) {
     signal.Turn(true, false, false, unit8_t 00000000, unit8_t 11111111, unit8_t 00000000);
   }
-  while ( (interrupt >= 69) && (interrupt < 80) ) {
+  while ( (timer >= 69) && (timer < 80) ) {
     signal.Turn(false, true, true, unit8_t 00000000, unit8_t 11111111, unit8_t 00000000);
   }
-  while ( (interrupt >= 80) && (interrupt < 87) ) {
+  while ( (timer >= 80) && (timer < 87) ) {
     signal.Turn(false, false, false, unit8_t 00000000, unit8_t 00000000, unit8_t 00000000);
   }
-  while (interrupt < 104);
-  while ( (interrupt >= 104) && (interrupt < 108) ) {
+  while (timer < 104);
+  while ( (timer >= 104) && (timer < 108) ) {
     signal.CollisionAvoidance();
   }
-  while (interrupt < 200);
+  while (timer < 200);
 }
 
 void pause(int dur) {
