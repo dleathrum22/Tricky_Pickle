f1a5f265fd293fdfaecdfe5623cdb2fb0428c70e
dleathrum22 <leathd@rpi.edu>
2016-11-29 11:20:32 -0500

    Update IEDcode_v1.ino

--- IEDcode_v1.ino
+++ IEDcode_v1.ino
@@ -1,70 +1,59 @@
+#include <ctime>
+#include <iostream>
+
+#include "DisplaySignalMaker.h"
+
 void setup() {
   // put your setup code here, to run once:
   
   //constant variables
-  const int leftPin = 1;
-  const int rightPin = 2;
-  const int topPin = 3;
-  const int bottomPin = 4;
-  
-  
+  DisplaySignalMaker signal = new DisplaySignalMaker();
+  int interrupt;
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  //Speed will be a value between 0 and 255 for the speed of the LED flash
-}
-
-
-void LeftBlink(int speed) {
-  if (speed < 0) { speed = 0; }
-  else if (speed > 255) { speed = 255; }
-  analogWrite(leftPin, speed);
-}
-
-void RightBlink(int speed) {
-  if (speed < 0) { speed = 0; }
-  else if (speed > 255) { speed = 255; }
-  analogWrite(rightPin, speed);
-}
-
-void FullBlink(int speed) {
-  if (speed < 0) { speed = 0; }
-  else if (speed > 255) { speed = 255; }
-  analogWrite(leftPin, speed);
-  analogWrite(rightPin, speed);
-  analogWrite(topPin, speed);
-  analogWrite(bottomPin, speed);
-}
-
-void TopBlink(int speed) {
-  if (speed < 0) { speed = 0; }
-  else if (speed > 255) { speed = 255; }
-  analogWrite(topPin, speed);
-}
-
-void BottomBlink(int speed) {
-  if (speed < 0) { speed = 0; }
-  else if (speed > 255) { speed = 255; }
-  analogWrite(bottomPin, speed);
-}
-
-void RainbowTest() {
-  
-}
-
-void FadingTest(int speed) {
-  if (speed < 0) { speed = 0; }
-  else if (speed > 255) { speed = 255; }
-  while (speed > 0) {
-    analogWrite(leftPin, speed);
-    analogWrite(rightPin, speed);
-    analogWrite(topPin, speed);
-    analogWrite(bottomPin, speed);
-    speed =- 10;
+  while (interrupt < 10);
+  while ( (interrupt >= 10) && (interrupt < 21)) {
+    signal.Turn(false, true, true, unit8_t 00000000, unit8_t 11111111, unit8_t 00000000);
   }
+  while ( (interrupt >= 22) && (interrupt < 28) ) {
+    signal.Turn(true, false, false, unit8_t 00000000, unit8_t 11111111, unit8_t 00000000);
+  }
+  while ( (interrupt >= 28) && (interrupt < 36) ) {
+    signal.Turn(true, false, true, unit8_t 00000000, unit8_t 11111111, unit8_t 00000000);
+  }
+  while (interrupt < 37);
+  while ( (interrupt >= 37) && (interrupt < 40) ) {
+    signal.Turn(true, false, true, unit8_t 00000000, unit8_t 11111111, unit8_t 00000000);
+  }
+  while ( (interrupt >= 40) && (interrupt < 52) ) {
+    signal.Turn(false, true, true, unit8_t 00000000, unit8_t 11111111, unit8_t 00000000);
+  }
+  while (interrupt < 54);
+  while ( (interrupt >= 54) && (interrupt < 61) ) {
+    signal.Turn(false, true, false, unit8_t 11111111, unit8_t 11111111, unit8_t 00000000);
+  }
+  while ( (interrupt >= 61) && (interrupt < 69) ) {
+    signal.Turn(true, false, false, unit8_t 00000000, unit8_t 11111111, unit8_t 00000000);
+  }
+  while ( (interrupt >= 69) && (interrupt < 80) ) {
+    signal.Turn(false, true, true, unit8_t 00000000, unit8_t 11111111, unit8_t 00000000);
+  }
+  while ( (interrupt >= 80) && (interrupt < 87) ) {
+    signal.Turn(false, false, false, unit8_t 00000000, unit8_t 00000000, unit8_t 00000000);
+  }
+  while (interrupt < 104);
+  while ( (interrupt >= 104) && (interrupt < 108) ) {
+    signal.CollisionAvoidance();
+  }
+  while (interrupt < 200);
+}
+
+void pause(int dur) {
+  int temp = time(NULL) + dur;
+  while(temp > time(NULL));
 }
 
 /*
