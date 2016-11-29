89fa47a4e3e8267d39de16d1a0a7b0bdc944ceb9
dleathrum22 <leathd@rpi.edu>
2016-11-29 15:07:43 -0500

    Update IEDcode_v1.ino

--- IEDcode_v1.ino
+++ IEDcode_v1.ino
@@ -2,61 +2,103 @@
 #include <iostream>
 
 #include "DisplaySignalMaker.h"
+#include "Timer.h"
 
 void setup() {
   // put your setup code here, to run once:
   
   //constant variables
   DisplaySignalMaker signal = new DisplaySignalMaker();
-  int timer = timer(0);
+  time_t timer = now();
+  
+  int next;
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  while (timer < 10);
-  while ( (timer >= 10) && (timer < 21)) {
+  next = time_t.second(timer) + 10;
+  while (sec < next) { timer = now(); }
+  
+  next = next + 11;
+  while ( time_t.second(timer) < next ) {
     signal.Turn(false, true, true, unit8_t 00000000, unit8_t 11111111, unit8_t 00000000);
+    timer = now();
   }
-  while ( (timer >= 22) && (timer < 28) ) {
+  
+  next = next + 4
+  while ( time_t.second(timer) < next ) {
     signal.Turn(true, false, false, unit8_t 00000000, unit8_t 11111111, unit8_t 00000000);
+    timer = now(); 
   }
-  while ( (timer >= 28) && (timer < 36) ) {
+  
+  next = next + 8;
+  while ( time_t.second(timer) < next ) {
     signal.Turn(true, false, true, unit8_t 00000000, unit8_t 11111111, unit8_t 00000000);
+    timer = now(); 
   }
-  while (timer < 37);
-  while ( (timer >= 37) && (timer < 40) ) {
+  
+  next = next + 1;
+  while (time_t.second(timer) < next) { timer = now(); }
+  
+  next = next + 3
+  while ( time_t.second(timer) < next ) {
     signal.Turn(true, false, true, unit8_t 00000000, unit8_t 11111111, unit8_t 00000000);
+    timer = now(); 
   }
-  while ( (timer >= 40) && (timer < 52) ) {
+  
+  next = next + 12;
+  while ( time_t.second(timer) < next ) {
     signal.Turn(false, true, true, unit8_t 00000000, unit8_t 11111111, unit8_t 00000000);
+    timer = now(); 
   }
-  while (timer < 54);
-  while ( (timer >= 54) && (timer < 61) ) {
+  
+  next = next + 2;
+  while (time_t.second(timer) < next) { timer = now(); }
+  
+  next = next + 7;
+  while ( time_t.second(timer) < next ) {
     signal.Turn(false, true, false, unit8_t 11111111, unit8_t 11111111, unit8_t 00000000);
+    timer = now(); 
   }
-  while ( (timer >= 61) && (timer < 69) ) {
+  
+  next = next + 8;
+  while ( time_t.second(timer) < next ) {
     signal.Turn(true, false, false, unit8_t 00000000, unit8_t 11111111, unit8_t 00000000);
+    timer = now(); 
   }
-  while ( (timer >= 69) && (timer < 80) ) {
+  
+  next = next + 11;
+  while ( time_t.second(timer) < next ) {
     signal.Turn(false, true, true, unit8_t 00000000, unit8_t 11111111, unit8_t 00000000);
+    timer = now();
   }
-  while ( (timer >= 80) && (timer < 87) ) {
+  
+  next = next + 7;
+  while ( time_t.second(timer) < next ) {
     signal.Turn(false, false, false, unit8_t 00000000, unit8_t 00000000, unit8_t 00000000);
+    timer = now();
   }
-  while (timer < 104);
-  while ( (timer >= 104) && (timer < 108) ) {
+  
+  next = next + 17;
+  while (time_t.second(timer) < next) { timer = now(); }
+  
+  next = next + 4
+  while ( time_t.second(timer) < next ) {
     signal.CollisionAvoidance();
+    timer = now();
   }
-  while (timer < 200);
+  
+  next = next + 100
+  while (time_t.second(timer) < next) { timer = now(); }
 }
-
+/*
 void pause(int dur) {
   int temp = time(NULL) + dur;
   while(temp > time(NULL));
 }
 
-/*
+
 dot moving in circle
 flash
     Lelft/Right
